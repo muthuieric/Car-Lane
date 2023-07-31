ed40af08c99fa9390f3a70a56a388693b77f5d15
Eric <ericmuthuipatch22@gmail.com>
2023-07-31 18:32:44 +0300

    hero page completed

--- car-lane/src/components/HeroSection.js
+++ car-lane/src/components/HeroSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import carImage from '../images/carImage.jpeg';
+import carImage from '../images/Shmes-Zenvo-TSR-S.avif';
 
 const HeroSection = () => {
   return (
@@ -8,7 +8,7 @@ const HeroSection = () => {
       {/* Navbar */}
       <nav className="fixed top-0 left-0 right-0 flex justify-between items-center py-4 px-8 bg-purple-500 z-10">
         <div className="text-white font-bold text-2xl">CarLane</div>
-        <ul className="flex space-x-4 text-white text-xl font-semibold">
+        <ul className="flex space-x-4 text-white text-xl font-medium">
           <li className="hover:bg-purple-600 px-4 py-2 rounded">Home</li>
           <li className="hover:bg-purple-600 px-4 py-2 rounded">Cars</li>
           <li className="hover:bg-purple-600 px-4 py-2 rounded">Services</li>
