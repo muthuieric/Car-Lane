2533cedb234441a2649e587f1b75a77b4ac6c952
Eric <ericmuthuipatch22@gmail.com>
2023-07-31 18:09:01 +0300

    initial commit

---
+++ car-lane/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from './components/HeroSection';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
