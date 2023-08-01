202d8d9029675d3c9c0e355b4952dc8b58b9f183
Eric <ericmuthuipatch22@gmail.com>
2023-08-01 19:21:44 +0300

    styling contact page

--- car-lane/src/components/Contact.js
+++ car-lane/src/components/Contact.js
@@ -1,13 +1,18 @@
+import React from 'react';
 import Form from "./Form";
 import About from "./About";
 
 const Contact = () => {
-    return (
-        <div className="flex flex-row flex-wrap content-around">
-        <About/>
+  return (
+    <div className="flex flex-row flex-wrap justify-center pt-22">
+      <div className="flex-1 p-4">
+        <About />
+      </div>
+      <div className="flex-1 p-4">
         <Form />
-        </div>
-      );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Contact;
