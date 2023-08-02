8f12a47eda997f969097cb5aec313d2cdd05e4d7
Eric <ericmuthuipatch22@gmail.com>
2023-08-02 16:08:04 +0300

    added art

--- car-lane/src/components/ArtWorkItem.js
+++ car-lane/src/components/ArtWorkItem.js
@@ -2,18 +2,26 @@ import React from 'react';
 
 const ArtworkItem = ({ artwork }) => {
   return (
-<div key={artwork.id} className="mt-8 bg-white rounded h-full w-60 m-2 border-2 flex flex-col ">
+    <div key={artwork.id} className="mt-8 bg-white rounded h-100 w-80 m-2 border-2 flex flex-col">
       {artwork.image_id && (
         <img
           src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
           alt={artwork.title}
-          className='rounded w-60 h-40  object-cover'
+          className="rounded h-52 w-80 object-cover"
         />
       )}
-      <h2>{artwork.title}</h2>
-
+      <div className="p-4 flex flex-col justify-between">
+        <div>
+          <p className="font-bold ">Title: {artwork.title}</p>
+          <p className="italic ">Artist: {artwork.artist_title}</p>
+        </div>
+        <div>
+          <p className="">Origin: {artwork.place_of_origin}</p>
+          <p className="">Year: {artwork.date_display}</p>
+        </div>
+      </div>
     </div>
-  )};
+  );
+};
 
 export default ArtworkItem;
-
