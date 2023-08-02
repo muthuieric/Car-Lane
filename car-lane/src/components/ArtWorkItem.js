8fe3d83eb5b61748971d298a00fc23b1f4a03a11
Eric <ericmuthuipatch22@gmail.com>
2023-08-03 00:16:05 +0300

    modifying route parameters

--- car-lane/src/components/ArtWorkItem.js
+++ car-lane/src/components/ArtWorkItem.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 const ArtWorkItem = ({ artwork }) => {
   return (
     <div key={artwork.id} className="mt-8 bg-white rounded h-100 w-80 m-2 border-2 flex flex-col">
+      <Link to={`/art/${artwork.id}`}>
       {artwork.image_id && (
         <img
           src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
@@ -13,18 +14,15 @@ const ArtWorkItem = ({ artwork }) => {
       )}
       <div className="p-4 flex flex-col justify-between ">
         <div>
-        <Link to={`/art/${artwork.id}`}>
-
-          <p className="font-bold ">Title: {artwork.title}</p>
+          <p className="font-semibold ">Title: {artwork.title}</p>
           <p className="italic ">Artist: {artwork.artist_title}</p>
-      </Link>
-
         </div>
         <div>
           <p className="">Origin: {artwork.place_of_origin}</p>
           <p className="">Year: {artwork.date_display}</p>
         </div>
       </div>
+      </Link>
     </div>
   );
 };
