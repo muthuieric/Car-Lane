f565249a72f8fef757871d4137292328ecf5f553
Eric <ericmuthuipatch22@gmail.com>
2023-08-03 23:30:59 +0300

    finished

--- visionarygerm/src/components/Home.js
+++ visionarygerm/src/components/Home.js
@@ -8,10 +8,12 @@ const Home = () => {
       <div className="w-full h-screen">
         <img className="w-full h-full object-cover fixed inset-0" src={carImage} alt="Car" />
       </div>
-      
+
+ 
+
       {/* Hero Content */}
       <div className="fixed bottom-0 left-0 w-full px-8 py-6 bg-purple-500 bg-opacity-50">
-        <h1 className="text-4xl text-white font-bold m-2">Discover Artistic Marvels</h1>
+        <h1 className="text-4xl text-white font-bold m-2 ">Discover Artistic Marvels</h1>
         <p className="text-white text-lg mb-2">
         "Art is the lie that enables us to realize the truth." - Pablo Picasso
         </p>
@@ -25,6 +27,3 @@ export default Home;
 
 
 
- {/* <button className="bg-white text-purple-500 px-6 py-3 rounded-lg text-lg font-semibold">
-          Explore Art
-        </button> */}
--- visionarygerm/src/components/NavBar.js
+++ visionarygerm/src/components/NavBar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     <>
       <nav className="fixed top-0 left-0 right-0 flex justify-between items-center py-4 px-8 bg-purple-500 z-10">
         <NavLink to="/" className="text-white font-bold text-2xl">VisonaryGerm</NavLink>
-        <div className={`flex flex-col md:flex-row md:space-x-4 ${open ? 'block' : 'hidden md:block'}`}>
+        <div className={`flex flex-col md:flex-row md:space-x-4  ${open ? 'block' : 'hidden md:block'}`}>
           <NavLink exact to="/" className="text-white text-xl font-medium hover:bg-purple-600 px-4 py-2 rounded" activeClassName="bg-purple-600">Home</NavLink>
           <NavLink to="/art" className="text-white text-xl font-medium hover:bg-purple-600 px-4 py-2 rounded" activeClassName="bg-purple-600">Art</NavLink>
           <NavLink to="/contacts" className="text-white text-xl font-medium hover:bg-purple-600 px-4 py-2 rounded" activeClassName="bg-purple-600">Contacts</NavLink>
